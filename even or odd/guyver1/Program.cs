70868d6d3a3c464ec9f25833fe383622e9121f12
happypoofysquirrel <guyver432@gmail.com>
2015-10-06 20:31:40 -0400

    new

---
+++ even or odd/guyver1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guyver1
+{
+    class Program
+    {
+        private static object sd;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see if it is even or odd");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x % 2 == 0)
+
+            {
+
+                Console.WriteLine("even");
+            } 
+
+            
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+        }
+    }
+}
