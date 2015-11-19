58913730c6c8adb255a8981e34ca95404c940072
happypoofysquirrel <guyver432@gmail.com>
2015-11-19 13:53:00 -0500

    start

--- fizzbuzz/fizzbuzz/Program.cs
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -10,46 +10,14 @@ namespace fizzbuzz
     {
         static void Main(string[] args)
         {
-  /*          Console.WriteLine("enter a value for Fizz");
-            int fizz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a value for Fizz");
+             int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter a value for buzz");
-            int buzz = Convert.ToInt32(Console.ReadLine());
+             int y = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("what number do you want to count to?");
-            int count = Convert.ToInt32(Console.ReadLine());
+             int n = Convert.ToInt32(Console.ReadLine());
 
-            //int[] fb = new int[count];
-
-            int a;
-            for (a = 1; a <= count; a++)
-            {
-                if (a % fizz == 0 && a % buzz == 0)
-                { 
-                    Console.Write(" FB ");
-                }
-
-                else if (a % fizz == 0)
-                {
-                    Console.Write(" F ");
-                }
-                else if (a % buzz == 0)
-                {
-                    Console.Write(" B ");
-                }
-
-                else
-                {
-                    Console.Write(a);
-                }
-     */
-                   // entering in code eval
-                    string line= "3 5 20";
-            string[] myVals = line.Split(' ');
-                int x=int.Parse(myVals[0]);
-                int y =int.Parse(myVals[1]);
-                int  n =int.Parse(myVals[2]);
-                
-                
-                
+        
                 int a;
             for (a = 1; a <= n; a++)
             {
